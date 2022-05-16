8867eb52634ec9c80d0cbb8e0555746fb72f8c75
Mohak Jain <mohakjain55055@gmail.com>
2022-05-16 15:20:30 +0530

    CSS Almost Done

--- JavaScript/Main.js
+++ JavaScript/Main.js
@@ -1,1 +1,10 @@
 console.log("JS Running");
+const MobileMenuButton = document.getElementById("MobileMenuIcon");
+const NavBar = document.getElementById("Navbar");
+let MenuOpened = false;
+
+MobileMenuButton.addEventListener("click", () =>
+{
+    NavBar.style.display = MenuOpened ? "none" : "block";
+    MenuOpened = !MenuOpened;
+})
